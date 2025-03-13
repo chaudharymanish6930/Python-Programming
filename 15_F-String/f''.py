3a82f227cbf0d369b3804171acf98f22675c6441
Manish Choudhary <me.developer4u@gmail.com>
2025-03-13 14:01:19 +0530

    Add files via upload

---
+++ 15_F-String/f''.py
@@ -0,0 +1,10 @@
+country="india"
+name="manish"
+
+print(f"hey my name is {name} and i am from {country}")
+
+ountry="india"
+name="manish"
+
+print(f"hey my name is {{name}} and i am from {{country}}")
+
---
+++ 15_F-String/string_formating.py
@@ -0,0 +1,9 @@
+#formating string
+letter ="hey my namme is {} and i am from {}"
+country="india"
+name="manish"
+ # written in a sequence of the needed in tis case
+print(letter.format(country,name))
+
+print(f"hey my name is {name} and i am from {country}")
+
