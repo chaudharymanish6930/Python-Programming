40b8ef8075e8ab5e09bd255d76ccc461a2cf0b27
Manish Choudhary <me.developer4u@gmail.com>
2025-03-15 11:54:38 +0530

    Add files via upload

---
+++ 03_Conditional statement/05_if_else_in_one_line_shorthand.py
@@ -0,0 +1,15 @@
+a=330
+b=3303
+print("A") if a>b else print("=") if a==b else print("B")
+
+a=90
+b=8
+print("A") if a>b else ''
+print("=") if a==b else print("not equal")
+print("B") if a<b else 'not greater than a' # not print anything
+
+a=9
+b=9
+print("A") if a>b else ""
+print("b") if a==b else print("B")
+print("A") if a>b else print("=") if a==b else print("B")
