5a84595790e9d9865df40a6df1fa82c6bbb28c83
Manish Choudhary <me.developer4u@gmail.com>
2025-03-15 10:29:49 +0530

    Add files via upload

---
+++ 17_Exception Handling/03_divide_two_number.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+    result = num1 / num2
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Error: You cannot divide by zero!")
+except ValueError:
+    print("Error: Please enter a valid number!")
