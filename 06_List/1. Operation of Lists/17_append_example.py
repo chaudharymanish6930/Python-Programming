a075635cc31dd710c152197ee7e05f513d78aa08
Manish Choudhary <me.developer4u@gmail.com>
2025-03-18 10:34:17 +0530

    Add files via upload

---
+++ 06_List/1. Operation of Lists/17_append_example.py
@@ -0,0 +1,16 @@
+mov1=input("enter a movie name:")
+mov2=input("enter a movie name:")
+mov3=input("enter a movie name:")
+
+movie=[]
+
+movie.append(mov1)
+movie.append(mov2) 
+movie.append(mov3) 
+print(movie)
+
+movie2=[]
+movie2.append(input("enter an another movie: "))
+movie2.append(input("enter an another movie: "))
+movie2.append(input("enter an another movie: "))
+print(movie2)
