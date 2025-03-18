08e2ed9266cbb2fca78d66acc9ccb1b3403e4bc1
Manish Choudhary <me.developer4u@gmail.com>
2025-03-18 13:56:36 +0530

    Add files via upload

---
+++ 08_Dictionary/3. Nested Dictionary/04_update_the_dict.py
@@ -0,0 +1,12 @@
+marks = {}
+
+x=int(input("enter marks of physics: "))
+marks.update({"Physics":x})
+
+y=int(input("enter marks of chemistry: "))
+marks.update({"Chemistry":y})       
+
+z=int(input("enter marks of maths: "))
+marks.update({"Maths":z})       
+
+print(marks)
