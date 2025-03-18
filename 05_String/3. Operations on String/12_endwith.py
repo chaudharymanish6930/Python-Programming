8c76f433e0e831704a9c4da9060e186db412ebf7
Manish Choudhary <me.developer4u@gmail.com>
2025-03-18 10:07:46 +0530

    Add files via upload

---
+++ 05_String/3. Operations on String/12_endwith.py
@@ -0,0 +1,7 @@
+x="Manish Choudhary"
+print(x.endswith("hary"))  #True
+print(x.endswith("Choudhary"))     #True
+print(x.endswith("Manish"))     #False  #It is case sensitive 
+
+y=x.endswith("Choudhary")
+print(y)
