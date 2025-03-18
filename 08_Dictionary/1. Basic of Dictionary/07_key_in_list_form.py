14c14b35e80b6b2a1f0f13dd2fdc68f6e39f8eee
Manish Choudhary <me.developer4u@gmail.com>
2025-03-18 11:30:41 +0530

    Add files via upload

---
+++ 08_Dictionary/1. Basic of Dictionary/07_key_in_list_form.py
@@ -0,0 +1,17 @@
+dict ={
+    "name": "Sakib",
+    "class": "10th",
+    "subject":{
+        "sub1":"maths",
+        "sub2":"english",
+        "sub3":"sst"
+    }
+}
+print(list(dict.keys()))
+print(list(dict.values()))
+
+print(tuple(dict.keys()))
+print(tuple(dict.values()))
+
+# print(set(dict.keys()))
+# print(set(dict.values()))
