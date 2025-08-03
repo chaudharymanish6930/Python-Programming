59dd1d11499ac3b77cf6c859b6fc0be724f0202f
Manish Choudhary <me.developer4u@gmail.com>
2025-08-03 20:36:35 +0530

    fourth

--- 01_Data type/Boolean Data type/boolean.py
+++ 01_Data type/Boolean Data type/boolean.py
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 a=True
 b=False
 print(a)
 print(b)
 print(type(a))
-=======
-a=True
+
+a=True 
 b=False
 print(a)
 print(b)
 print(type(a))
->>>>>>> 85a1649 (first)
+# >>>>>>> 85a1649 (first)
 print(type(b))
