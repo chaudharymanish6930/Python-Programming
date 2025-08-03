3a9fadf32a52930f7b02fe719bed6331cde2d4c7
Manish Choudhary <me.developer4u@gmail.com>
2025-08-03 14:24:14 +0530

    by owner

--- 09_Sets/1._Operation on Sets/02_clear.py
+++ 09_Sets/1._Operation on Sets/02_clear.py
@@ -7,4 +7,8 @@ s1={1,2,3,4,5,8,6,7,}
 s1.clear
 # This code clears all elements from the set s1
 print(s1)
+<<<<<<< HEAD
 >>>>>>> 85a1649 (first)
+=======
+print("Set after clear operation:", s1)
+>>>>>>> c68e5d1 (by owner)
