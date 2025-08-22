a5155ff30c301d28bdfd0a997f8494f96f7bcdf5
Manish Choudhary <me.developer4u@gmail.com>
2025-08-22 12:38:39 +0530

    Update

--- 05_String/3. Operations on String/04_uppercase.py
+++ 05_String/3. Operations on String/04_uppercase.py
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 message = "Hello, Python Programming!"
 
 print("Uppercase:", message.upper())   # Convert to uppercase
 print("Lowercase:", message.lower())   # Convert to lowercase
 print("Title Case:", message.title())  # Convert to title case
-=======
+# =======
 message = "Hello, Python Programming!"
 
 print("Uppercase:", message.upper())   # Convert to uppercase
 print("Lowercase:", message.lower())   # Convert to lowercase
 print("Title Case:", message.title())  # Convert to title case
->>>>>>> 85a1649 (first)
+# >>>>>>> 85a1649 (first)
