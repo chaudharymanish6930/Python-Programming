aa64a076afa251600c6181c94fa256d2404c271a
Manish Choudhary <me.developer4u@gmail.com>
2025-08-22 16:29:38 +0530

    Update

--- 00_Basic Programs/01_leap_year_1.py
+++ 00_Basic Programs/01_leap_year_1.py
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 x=int(input("enter the year :"))
 if (x%4==0 and x%100!=0) or (x%400==0):
     print("the given year is leap year")
 else:
-=======
+    print("it is not a leap year")
+
 x=int(input("enter the year :"))
 if (x%4==0 and x%100!=0) or (x%400==0):
     print("the given year is leap year")
 else:
->>>>>>> 85a1649 (first)
     print("it is not a leap year")
