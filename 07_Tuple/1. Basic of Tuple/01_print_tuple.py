2c2017f15a17a5fc06cf8c2d86d41d21329067c0
Manish Choudhary <me.developer4u@gmail.com>
2025-08-23 08:42:54 +0530

    Update

--- 07_Tuple/1. Basic of Tuple/01_print_tuple.py
+++ 07_Tuple/1. Basic of Tuple/01_print_tuple.py
@@ -1,2 +1,7 @@
+# only count and index methods are available for tuple
+# tuple is immutable
+# tuple is ordered collection of items
+# tuple allows duplicate values
+# tuple is defined by using parenthesis()
 t1=(1,2,3,4,5)
 print(t1)
--- 09_Sets/1._Operation on Sets/01_add.py
+++ 09_Sets/1._Operation on Sets/01_add.py
@@ -3,5 +3,9 @@
 s1={1,2,3}
 s1.add(4)
 print(s1)
-
 #sets is mutable but set's elements are immutable 
+
+s1.add((5,6))  # tuple can be added
+print(s1)
+#s1.add([7,8])  # list cannot be added
+#print(s1)  
