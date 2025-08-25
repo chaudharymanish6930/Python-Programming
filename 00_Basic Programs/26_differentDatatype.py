7f5b9ae066c13c049b5f645b946ad71f35b25ed2
Manish Choudhary <me.developer4u@gmail.com>
2025-08-25 23:32:09 +0530

    Update

--- 00_Basic Programs/26_differentDatatype.py
+++ 00_Basic Programs/26_differentDatatype.py
@@ -6,3 +6,30 @@ data_types = [
     "bool",
     "bytes", "bytearray", "memoryview"
 ]
+l_str = []
+l_bool = []
+l_dict = []
+l_byte = []
+l_list = []
+l_set = []
+for i in data_types:
+    if type(i) == str:
+        l_str.append(i)
+    elif type(i) == bool:
+        l_bool.append(i)
+    elif type(i) == dict:
+        l_dict.append(i)
+    elif type(i) == bytes:
+        l_byte.append(i)
+    elif type(i) == list:
+        l_list.append(i)    
+    elif type(i) == set:
+        l_set.append(i)
+
+print("String Data Types: ", l_str)
+print("Boolean Data Types: ", l_bool)
+print("Dictionary Data Types: ", l_dict)
+print("Byte Data Types: ", l_byte)
+print("List Data Types: ", l_list)  
+print("Set Data Types: ", l_set)
+        
