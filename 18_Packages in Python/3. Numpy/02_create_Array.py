cfc501e0a842d50c6e6938051510fdcd4f467d78
Manish Choudhary <me.developer4u@gmail.com>
2025-09-24 17:56:53 +0530

    Update

---
+++ 18_Packages in Python/3. Numpy/02_create_Array.py
@@ -0,0 +1,9 @@
+import numpy as np
+# Create a numpy array
+print(np.array([1,2,3,4,5]))
+
+# createe 2-d array
+print(np.array([[1,3,5],[2,4,6]]))
+
+# create 3-D array
+print(np.array([[1,2,3],[6,7,8],[9,8,7]]))
